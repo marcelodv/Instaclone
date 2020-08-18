9a4d4cf18e9a09447d80c3cb4b90cb4e90bb0463
Marcelo Santos <marcelo.santos.dev@gmail.com>
2020-08-18 02:18:54 -0300

    Add Refresh Action

--- src/Feed/index.js
+++ src/Feed/index.js
@@ -1,31 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { View, FlatList } from 'react-native';
 
-import { Post, Header, Avatar, Name, PostImage, Description } from './styles'
+import { Post, Header, Avatar, Name, PostImage, Description, Loading } from './styles'
 
 
 export default function Feed() {
   const [feed, setFeed] = useState([])
+  const [page, setPage] = useState(1)
+  const [total, setTotal] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [refreshing, setRefreshing] = useState(false)
+
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
+    if(total && pageNumber > total) return;
+
+    setLoading(true)
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limits=5&_page=${pageNumber}`
+      )
+
+    const data = await response.json()
+    const totalItems = response.headers.get('X-Total-Count')
+
+    setTotal(Math.floor(totalItems / 5))
+    setFeed(shouldRefresh ? data : [...feed, ...data])
+    setPage(pageNumber + 1)
+    setLoading(false)
+  } 
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limits=5&_page=1'
-        )
-
-      const data = await response.json()
-
-      setFeed(data)
-    }
-
-    loadFeed()
+    loadPage()
   }, [])
 
+  async function refreshList() {
+    setRefreshing(true)
+
+    await loadPage(1, true)
+
+    setRefreshing(false)
+  }
 
   return (
     <View>
       <FlatList 
         data={feed} 
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
         keyExtractor={post => String(post.id)} 
         renderItem={({ item }) => (
           <Post>
